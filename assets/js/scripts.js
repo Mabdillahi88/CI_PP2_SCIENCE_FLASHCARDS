2acf392b49d5bf0ccf62d1646a31c9bd6d2e67c0
Mabdillahi88 <mohamedabdillahi2@hotmail.com>
2023-06-01 22:34:53 +0000

    Add biology flash cards series with keywords and definitions

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -3,3 +3,8 @@ const flashCardsSeries = {
     physics: [
         { keyword: 'Force', definition: 'A push or pull upon an object resulting from the object\'s interaction with another object.' },
         { keyword: 'Energy', definition: 'The capacity to do work.' },
+        // add more Physics flashcards here...
+    ],
+    biology: [
+        { keyword: 'Cell', definition: 'The basic structural, functional, and biological unit of all known organisms.' },
+        { keyword: 'DNA', definition: 'A molecule composed of two chains that coil around each other to form a double helix.' },
