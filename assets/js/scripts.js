b2e62a6d51722bf2c3ea4da4d39684a7c33255e6
Mabdillahi88 <mohamedabdillahi2@hotmail.com>
2023-06-01 22:34:03 +0000

    Add flash cards series object for storing flash cards organized by topic

---
+++ assets/js/scripts.js
@@ -0,0 +1,5 @@
+// An object to store our flash cards arrays, organized by topic
+const flashCardsSeries = {
+    physics: [
+        { keyword: 'Force', definition: 'A push or pull upon an object resulting from the object\'s interaction with another object.' },
+        { keyword: 'Energy', definition: 'The capacity to do work.' },
